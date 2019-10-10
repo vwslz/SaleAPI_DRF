862fec66db5ddde817ddd8fe9bea2aff16e675ac
vwslz <vwslzk@gmail.com>
2019-10-10 14:22:32 -0400

    GCBS Succeed.

--- SaleWeb/sale/urls.py
+++ SaleWeb/sale/urls.py
@@ -1,7 +1,20 @@
+# from django.urls import path
+# from sale import views
+#
+# urlpatterns = [
+#     path('sale/', views.SKUList),
+#     path('sale/<int:pk>/', views.SKUDetail),
+# ]
+
+
 from django.urls import path
+from django.urls import re_path
+from rest_framework.urlpatterns import format_suffix_patterns
 from sale import views
 
 urlpatterns = [
-    path('sale/', views.SKU_list),
-    path('sale/<int:pk>/', views.SKU_detail),
+    path('sale/', views.SKUList.as_view()),
+    path('sale/<int:pk>/', views.SKUDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- SaleWeb/sale/views.py
+++ SaleWeb/sale/views.py
@@ -2,45 +2,59 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
-from rest_framework import generics
+# from sale.models import SKU
+# from sale.serializers import SKUSerializer
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+#
+# def SKUList(APIView):
+#     def get(self, request, format=None):
+#         sku = SKU.objects.all()
+#         serializer = SKUSerializer(sku, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = SKUSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+# class SKUDetail(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return SKU.objects.get(pk=pk)
+#         except SKU.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         sale = self.get_object(pk)
+#         serializer = SKUSerializer(sale)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         sku = self.get_object(pk)
+#         serializer = SKUSerializer(sku, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         sku = self.get_object(pk)
+#         sku.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
 from sale.models import SKU
 from sale.serializers import SKUSerializer
+from rest_framework import generics
 
-def SKU_list(request):
-    if request.method == 'GET':
-        snippets = SKU.objects.all()
-        serializer = SKUSerializer(snippets, many=True)
-        return JsonResponse(serializer.data, safe=False)
+class SKUList(generics.ListCreateAPIView):
+    queryset = SKU.objects.all()
+    serializer_class = SKUSerializer
 
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = SKUSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-def SKU_detail(request, pk):
-    try:
-        snippet = SKU.objects.get(pk=pk)
-    except SKU.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = SKUSerializer(snippet)
-        return JsonResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = SKUSerializer(snippet, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        snippet.delete()
-        return HttpResponse(status=204)
+class SKUDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SKU.objects.all()
+    serializer_class = SKUSerializer
